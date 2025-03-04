cbb84fce58bec0b05db1c29d2b101960dc8c9c84
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-04 10:10:28 +0330

    build login page ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Login from "./pages/login.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+            </Routes>
+        </>
+    )
 }
 
 export default App
