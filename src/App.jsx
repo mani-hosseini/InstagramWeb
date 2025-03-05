b2f3cc8aa89f9dddc952cb02e08fc99c1b81e759
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-05 09:18:16 +0330

    upate routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import {useState} from 'react'
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import Home from "./pages/home.jsx";
 
 function App() {
     return (
-        <>
+        <Router>
             <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/signup" element={<Signup/>}/>
-                <Route path="/home" element={<Home/>}/>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
-        </>
-    )
+        </Router>
+    );
 }
 
-export default App
+export default App;
