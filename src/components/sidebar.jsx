0cf096ff057fc8eff59c364c7a7e65823ff85710
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-05 12:13:33 +0330

    build search box in side-bar section

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,22 +4,22 @@ import {useState} from "react";
 
 const Sidebar = () => {
     const [show, setShow] = useState(true);
+    const [showDiv, setShowDiv] = useState(false);
 
     const toggle = () => {
-        console.log('Search Clicked!');
         setShow(!show);
+        setShowDiv(!showDiv);
     };
 
     return (
         <div
-            className={`p-4 fixed border-r border-r-gray-300 transition-all duration-300 ${show ? 'w-60' : 'w-16'}`}>
-            {
-                show ? (
-                    <img src="../../src/assets/signup/insta%20logo.png" className="mb-6" alt=""/>
-                ) : (
-                    <FaInstagram className="w-5 h-5 text-[#E4405F] mb-6"/>
-                )
-            }
+            className={`p-4 fixed bg-white shadow h-[600px] border-r border-r-gray-300 transition-all duration-300 ${show ? 'w-60' : 'w-16'}`}>
+            {show ? (
+                <img src="../../src/assets/signup/insta%20logo.png" className="mb-6" alt=""/>
+            ) : (
+                <FaInstagram className="w-5 h-5 text-[#E4405F] mb-6"/>
+            )}
+
             <ul className={`space-y-6 ${show ? 'mt-0' : 'mt-10'}`}>
                 <li>
                     <Link to="/home"
@@ -36,6 +36,22 @@ const Sidebar = () => {
                         {show && 'Search'}
                     </button>
                 </li>
+                {showDiv && (
+                    <div
+                        className="absolute top-0 left-full bg-white w-80 h-full p-4 shadow rounded-tr-xl rounded-br-xl border border-gray-300">
+                        <h2 className="text-lg font-semibold mb-8">Search</h2>
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full p-2 bg-[#EFEFEF] border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                        <div className="border-b border-gray-300 w-full mt-6"></div>
+                        <div className="flex justify-between text-sm mt-2">
+                            <button className=" cursor-pointer">Recent</button>
+                            <button className="text-blue-500 cursor-pointer">Clear all</button>
+                        </div>
+                    </div>
+                )}
                 <li>
                     <Link to="/create"
                           className="flex items-center gap-3 text-lg text-[#262626] hover:text-black transition-all">
